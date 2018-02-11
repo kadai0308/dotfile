e79354dab960c8db28969ef972bfa0846c797207
Kuan-Yu Chang <kychang009@gmail.com>
2018-02-11 16:43:58 +0800

    seperate the copying section

---
+++ copy_dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# copy dotfiles to home
+cp gitconfig $HOME/.gitconfig
+cp bash_profile $HOME/.bash_profile
+cp vimrc $HOME/.vimrc
+cp -r vim $HOME/.vim
+cp -r config $HOME/.config
--- new_env.sh
+++ new_env.sh
@@ -8,12 +8,7 @@ sudo hdiutil detach /Volumes/Google\ Chrome\ Incognito/
 rm $HOME/Downloads/GoogleChromeIncognito_v0.0.4.dmg
 
 # copy dotfiles to home
-cp gitconfig $HOME/.gitconfig
-cp bash_profile $HOME/.bash_profile
-cp vimrc $HOME/.vimrc
-cp -r vim $HOME/.vim
-cp -r config $HOME/.config
-
+./copy_dotfiles.sh
 
 # install brew
 ./install_brew.sh
