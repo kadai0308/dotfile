d7bedd8c583a93e4a879e53e3fa99a4c8705d7ea
Kuan-Yu Chang <kychang009@gmail.com>
2018-02-22 14:02:45 +0800

    add config of screen

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -3,6 +3,7 @@
 # copy dotfiles to home
 cp gitconfig $HOME/.gitconfig
 cp bash_profile $HOME/.bash_profile
+cp screenrc $HOME/.screenrc
 cp vimrc $HOME/.vimrc
 cp -r vim $HOME/.vim
 cp -r config $HOME/.config
