f0b27df7b1969bdbb3c7ab8ffc23b3bf8f79abf5
Kuan-Yu Chang <kychang009@gmail.com>
2018-09-25 10:29:58 +0800

    remove some setting for macOS 10.14

--- init_env.sh
+++ init_env.sh
@@ -6,13 +6,6 @@
 # install brew
 ./install_brew.sh
 
-# global config
-defaults write NSGlobalDomain AppleInterfaceStyle -string Dark
-
-# terminal
-defaults write com.apple.Terminal "Default Window Settings" -string Pro
-defaults write com.apple.Terminal "Startup Window Settings" -string Pro
-
 # menu bar display
 defaults write com.apple.menuextra.battery ShowPercent -string YES
 
