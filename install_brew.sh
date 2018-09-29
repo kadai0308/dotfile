5b0e46d080e0c1ab33dbc84c3c8c7b680e17fa3e
Kuan-Yu Chang <kychang009@gmail.com>
2018-09-29 23:36:57 +0800

    add plug-in for slack

--- install_brew.sh
+++ install_brew.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# extends the sudo timeout for another 15 minutes
+sudo -v
+
 # install brew
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -15,9 +18,15 @@ brew cask install xquartz
 brew cask install telegram-desktop
 brew cask install slack
 
+# install plug-in for slack
+curl -OL https://github.com/fsavje/math-with-slack/releases/download/v0.2.5/math-with-slack.sh
+sudo bash math-with-slack.sh
+rm math-with-slack.sh
+
 # install apps from brew
 brew install bash-completion
 brew install vim
 brew install unrar
 brew install translate-shell
 brew install openconnect
+
